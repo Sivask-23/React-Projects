29b40d912a64f7973cfd9fcc34dd637aa8dd792b
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2025-01-20 12:31:04 +0530

    Add files via upload

---
+++ CourseSite/src/Components/User pages/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../ComponentsCss/User Pages/UserDashboard.css'
+
+const UserDashboard = () => {
+  return (
+    <div className='outer-user-dash'>
+      UserDashboard
+    </div>
+  )
+}
+
+export default UserDashboard
---
+++ CourseSite/src/Components/User pages/UserMessages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../ComponentsCss/User Pages/UserMessages.css'
+
+const UserMessages = () => {
+  return (
+    <div className='outer-user-msg'>
+      UserMessages
+    </div>
+  )
+}
+
+export default UserMessages
